dd11393d22398786d00a6cb67875d3a08ec5639d
m-otomo <m-otomo@PWTNT2117>
2022-11-07 14:24:18 +0900

    2022/11/7

---
+++ BorrowBooks/Models/Book.cs
@@ -0,0 +1,12 @@
+﻿namespace BorrowBooks.Models
+{
+    public partial class Book
+    {
+        public int Book_ID { get; set; }
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public string Genre { get; set; }
+        public int? Lend_User_ID { get; set; }
+        public DateTime? Lend_Date { get; set; }
+    }
+}
