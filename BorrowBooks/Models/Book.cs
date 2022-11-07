b5091332d0e51651a8e327b1843b294be6591f1a
m-otomo <m-otomo@PWTNT2117>
2022-11-07 18:10:01 +0900

    2022/11/7

--- BorrowBooks/Models/Book.cs
+++ BorrowBooks/Models/Book.cs
@@ -1,12 +1,21 @@
-﻿namespace BorrowBooks.Models
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace BorrowBooks.Models
 {
     public partial class Book
     {
         public int Id { get; set; }
+        [Display(Name = "タイトル")]
         public string Title { get; set; }
+        [Display(Name = "著名")]
         public string Author { get; set; }
+        [Display(Name = "ジャンル")]
         public string Genre { get; set; }
+        [Display(Name = "貸出者ID")]
         public int? Lend_User_Id { get; set; }
+        [Display(Name = "貸出日")]
         public DateTime? Lend_Date { get; set; }
     }
 }
--- BorrowBooks/Models/User.cs
+++ BorrowBooks/Models/User.cs
@@ -1,11 +1,20 @@
-﻿namespace BorrowBooks.Models
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace BorrowBooks.Models
 {
     public partial class User
     {
+        
         public int Id { get; set; }
+        [Display(Name="氏名")]
         public string User_Name { get; set; }
+        [Display(Name = "メールアドレス")]
         public string Mail_Address { get; set; }
+        [Display(Name = "パスワード")]
         public string Password { get; set; }
+        [Display(Name = "電話番号")]
         public string Phone { get; set; }
     }
 }
